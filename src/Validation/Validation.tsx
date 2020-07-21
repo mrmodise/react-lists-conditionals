199e066f21fd1f64faa8403c310d1e417428d6fb
mrmodise <modisemorebodi@gmail.com>
2020-07-21 20:27:15 +0200

    fix: correct error message

--- src/Validation/Validation.tsx
+++ src/Validation/Validation.tsx
@@ -6,10 +6,10 @@ interface UserInput {
 }
 
 function Validation(props: UserInput) {
-    let validationMessage = 'Text long enough';
+    let validationMessage = 'Text too short';
 
     if (props?.userInputLength > 5) {
-        validationMessage = 'Text too short';
+        validationMessage = 'Text long enough';
     }
     return (
         <div className="App">
